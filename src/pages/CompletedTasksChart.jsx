686c320de7e518bdd1380cc2bada8e48698cc547
Raja Akash Kumar <rajaakashk9@gmail.com>
2025-02-24 12:28:24 +0530

    added pending task chart

--- src/pages/CompletedTasksChart.jsx
+++ src/pages/CompletedTasksChart.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { fetchTasks } from "../features/tasks/taskSlice";
-import { useEffect } from "react";
 import {
   Chart as ChartJS,
   BarElement,
@@ -10,6 +9,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 
@@ -17,25 +17,20 @@ ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const CompletedTasksChart = () => {
   const dispatch = useDispatch();
-  const { tasks } = useSelector((state) => state.tasks || []);
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
 
+  const { tasks } = useSelector((state) => state.tasks || []);
+
   // Filter completed tasks in the last 7 days
-  const lastWeekTasks = tasks?.filter(
+  // Filter completed tasks in the last 7 days
+  const lastWeekTasks = tasks.filter(
     (task) =>
       task.status === "Completed" &&
       moment(task.updatedAt).isAfter(moment().subtract(7, "days"))
   );
-
-  tasks.forEach((task) => {
-    console.log("UpdatedAt:", task.updatedAt);
-    console.log("Seven days ago:", moment().subtract(7, "days").toString());
-  });
-
-  console.log("Last week tasks:", lastWeekTasks);
 
   const data = {
     labels: lastWeekTasks.map((task) => task.name),
@@ -44,9 +39,58 @@ const CompletedTasksChart = () => {
         label: "Completed Tasks",
         data: lastWeekTasks.map(() => 1), // Each completed task counts as 1
         backgroundColor: "rgba(75, 192, 192, 0.6)",
+        barThickness: 30,
       },
     ],
   };
+
+  return <Bar data={data} />;
+  //   const completedTasks = tasks?.filter(
+  //     (task) =>
+  //       task.status === "Completed" &&
+  //       moment(task.updatedAt).isAfter(moment().subtract(7, "days"))
+  //   );
+
+  //   const todoTasks = tasks?.filter(
+  //     (task) =>
+  //       task.status === "To Do" &&
+  //       moment(task.updatedAt).isAfter(moment().subtract(7, "days"))
+  //   );
+  //   const blockedTasks = tasks?.filter(
+  //     (task) =>
+  //       task.status === "Blocked" &&
+  //       moment(task.updatedAt).isAfter(moment().subtract(7, "days"))
+  //   );
+
+  //   const data = {
+  //     labels: [
+  //       ...new Set([
+  //         ...completedTasks.map((task) => task.name),
+  //         ...todoTasks.map((task) => task.name),
+  //         ...blockedTasks.map((task) => task.name),
+  //       ]),
+  //     ], // Unique task names across all statuses
+  //     datasets: [
+  //       {
+  //         label: "Completed Tasks",
+  //         data: completedTasks.map(() => 1), // Each completed task counts as 1
+  //         backgroundColor: "rgba(75, 192, 192, 0.6)",
+  //         barThickness: 30,
+  //       },
+  //       {
+  //         label: "To Do Tasks",
+  //         data: todoTasks.map(() => 1), // Each to do task counts as 1
+  //         backgroundColor: "rgba(255, 99, 132, 0.6)",
+  //         barThickness: 30,
+  //       },
+  //       {
+  //         label: "Blocked Tasks",
+  //         data: blockedTasks.map(() => 1), // Each blocked task counts as 1
+  //         backgroundColor: "rgba(153, 102, 255, 0.6)",
+  //         barThickness: 30,
+  //       },
+  //     ],
+  //   };
 
   return <Bar data={data} />;
 };
--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import CompletedTasksChart from "./CompletedTasksChart";
-// import { fetchTasks } from "../features/tasks/taskSlice";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useEffect } from "react";
+import PendingTasksChart from "./PendingTasksChart";
 
 function ReportPage() {
-
   return (
     <>
       <main>
@@ -17,8 +14,27 @@ function ReportPage() {
             </div>
             <div className="col-md-10">
               <div className="container py-4">
-                <div>ReportPage</div>
-                <CompletedTasksChart />
+                <h1 className="mb-4">Report Page</h1>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="px-4">
+                      <h4>Total Work Completed Last Week</h4>
+                      <CompletedTasksChart />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="px-4">
+                      <h4>Tasks Closed by Each Team</h4>
+                      <CompletedTasksChart /> 
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="px-4">
+                      <h4>Pending Work Across Projects</h4>
+                      <PendingTasksChart />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
